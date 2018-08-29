d2afbf48e1ff8bcaec10435771ecc71e8e8d97a9
garikapatijaswitha <41731218+garikapatijaswitha@users.noreply.github.com>
2018-08-29 10:10:31 +0530

    Create 138.java

---
+++ 138.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	    static boolean isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+         
+        while (n != 1)
+        {
+            if (n % 2 != 0)
+                return false;
+            n = n / 2;
+        }
+        return true;
+    }
+ 
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+        if (isPowerOfTwo(n))
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
